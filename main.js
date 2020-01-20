bfb814a9207b8fac07782793d26d7b981fae82ab
castle <castlesmadeofcode@gmail.com>
2020-01-20 13:55:09 -0600

    Practice: Pet Sounds complete

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const petObj = {
+const fatCat = {
     name: "Fat Cat",
     species: "Dog",
     nicknames: ["buddy", "guy", "friend"],
-    age: 2
-
+    age: 2,
+    bark: function (something) {
+        console.log(`Fat Cat barks at ${something}`)
+    },
+    beg: function(treat) {
+        console.log(`Fat Cat begs for ${treat}`)
+    },
+    play: function(toy) {
+        console.log(`Fat Cat begs to play with ${toy}`)
+    }
 }
+
+fatCat.bark("mailmain");
+fatCat.beg("treats");
+fatCat.play("frisbee");
